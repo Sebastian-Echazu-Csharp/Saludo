89e314bfc4592d834f701ebf7d11e759ecdb79d8
Sebastian Echazú <sebastianechazu@outlook.com>
2021-09-08 11:20:40 -0300

    Agregar archivos de proyecto.

---
+++ Saludo/Program.cs
@@ -0,0 +1,10 @@
+﻿// Creamos la variable para almacenar el nombre
+String miNombre = "";
+// Solicitamos por consola que el usuario ingrese el nombre
+Console.WriteLine("Por favor, escriba su nombre");
+// asignamos a la variable miNombre el valor que ingreso el usuario
+miNombre = Console.ReadLine();
+// imprimimos por consola el saludo
+Console.WriteLine("Hola {0}", miNombre);
+// Para que no se cierre la aplicación de golpe
+Console.ReadKey();
